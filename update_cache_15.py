73dfe59281bbc9dc740e8b9a4e30d49065c179a4
Rajat sharma <rs617718@gmail.com>
2026-04-20 15:31:36 +0530

    Further format footer logo sizing, spacing, and strict left alignment

---
+++ update_cache_15.py
@@ -0,0 +1,20 @@
+import os
+import glob
+import re
+
+html_files = glob.glob('d:/New folder (3)/emporio/*.html')
+
+for file in html_files:
+    with open(file, 'r', encoding='utf-8') as f:
+        content = f.read()
+    
+    # regex to match style.css?v=...
+    pattern = r'style\.css\?v=[0-9\-]+'
+    new_version_string = 'style.css?v=20260420-15'
+    
+    new_content = re.sub(pattern, new_version_string, content)
+    
+    if new_content != content:
+        with open(file, 'w', encoding='utf-8') as f:
+            f.write(new_content)
+        print(f"Updated cache buster to 15 in {file}")
